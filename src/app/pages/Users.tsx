9bd1477571ebbe0381ef1f356a0d6136079beed1
algomachine007 <amaben202phantom@gmail.com>
2022-10-22 13:13:58 +0100

    installing babel preset for image loading

--- src/app/pages/Users.tsx
+++ src/app/pages/Users.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import useCtx from '../hooks/useCtx';
 
-const Users = () => {
-  const { users } = useCtx();
+import usersInDb from './../../data/data.json';
 
-  return <div>Users</div>;
+const Users = () => {
+  // const { users } = useCtx();
+
+  // @ts-ignore
+  // const allUsers = users.concat(usersInDb.users);
+
+  return (
+    <div>
+      {usersInDb.users &&
+        usersInDb.users.map((user) => (
+          <div key={user.id}>
+            <h1>{user.name}</h1>
+            <img src={user.avatarUrl} alt={user.name} />
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default Users;
