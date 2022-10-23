34725e490c038c78d5bbbed13855cd34588ffc86
algomachine007 <amaben202phantom@gmail.com>
2022-10-23 09:32:17 +0100

    updating image type

--- src/app/components/Card/index.tsx
+++ src/app/components/Card/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import style from './styles.module.css';
 
-const Card = ({ name, image }: { name: string; image: any }) => {
+const Card = ({ name, image }: { name: string; image: string }) => {
   const images = !image ? 'https://via.placeholder.com/400x300' : image;
-  console.log(image);
+
   return (
     <div className={style.card}>
       <img src={images} className={style.image} alt='' />
